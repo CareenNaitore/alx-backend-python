4cfead599756e8557c48e6d9874043a182d96cb1
careen naitore <careennaitore@gmail.com>
2023-01-05 15:08:43 +0300

    sum up mixed variables

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+""" how to sums mixed variables int and float"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[float,int]]) -> float:
+    """computes the sum of mixed variables"""
+
+    return float(sum(mxd_list))
