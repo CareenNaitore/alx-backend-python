50d843b807552269480fc6a971e2adc92f137936
careen naitore <careennaitore@gmail.com>
2023-01-05 14:59:55 +0300

    sum of lists of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+""" how to sum a list of floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """computes the sum of a floaing list"""
+
+    return float(sum(input_list))
