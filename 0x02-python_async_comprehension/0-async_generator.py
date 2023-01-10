e55e7ba805ab5f788a6f2bc0419691bcd31166b7
careen naitore <careennaitore@gmail.com>
2023-01-10 15:47:56 +0300

    generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""Task 0 """
 
-import random
-import asyncio
+""" Async Comprehensions """
+
+from asyncio import sleep
+from random import uniform
 from typing import Generator
 
 
-async def async_generator() -> [float, None, None]:
-    """Generates a sequence of ten numbers"""
+async def async_generator() -> Generator[float, None, None]:
+    """ Async Generator """
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.random() * 10
+        await sleep(1)
+        yield uniform(0, 10)
